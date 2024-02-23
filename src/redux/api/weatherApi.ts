a384aaac3deb1c10deee90ace4f13d52297e76a9
Vasyl <ve4nuy@rambler.ru>
2024-02-24 01:57:55 +0200

    add API key to .env

--- src/redux/api/weatherApi.ts
+++ src/redux/api/weatherApi.ts
@@ -1,12 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { WeatherResponse } from '../../types';
 
+const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
 export const weatherApi = createApi({
     reducerPath: 'weatherApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' }),
     endpoints: (builder) => ({
         fetchWeatherForCity: builder.query<WeatherResponse, { city: string }>({
-            query: ({ city }) => `${city}?unitGroup=metric&include=days%2Ccurrent&key=PQHCTEZAB6AFBBUSA3BNYC8WC&contentType=json`,
+            query: ({ city }) => `${city}?unitGroup=metric&include=days%2Ccurrent&key=${apiKey}&contentType=json`,
         }),
     }),
 });
